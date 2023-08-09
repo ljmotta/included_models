30bb31c0213b58a3f627b5e05b53ebf107727b4b
Luiz Motta <luizjoaomotta@gmail.com>
2023-08-09 08:52:52 -0300

    Initial commit

---
+++ target/classes/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "18e43dd5-9bf6-446e-be5d-7ff19f1b1930";
+}
---
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "e23e6aff-c2ed-48fd-9630-046c46562d81";
+}
