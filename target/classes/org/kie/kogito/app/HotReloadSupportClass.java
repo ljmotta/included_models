9198198c4088b405143df1100b991641f18a4750
Luiz Motta <luizjoaomotta@gmail.com>
2023-08-09 09:24:06 -0300

    Re import the model to re-add locationURI param

--- target/classes/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/classes/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "4738c3a1-ccd1-40c5-83ec-6d5cf7ba1e52";
+private static final String ID = "9a9cf04f-52b9-4af5-a983-f27cee7e3911";
 }
