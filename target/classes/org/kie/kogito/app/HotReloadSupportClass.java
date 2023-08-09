d5f87294ce8e214cf68da0083e3cd226b2ebd515
Luiz Motta <luizjoaomotta@gmail.com>
2023-08-09 09:52:12 -0300

    Fix second dmn

--- target/classes/org/kie/kogito/app/HotReloadSupportClass.java
+++
@@ -1,5 +0,0 @@
-package org.kie.kogito.app;
-@io.quarkus.runtime.Startup()
-public class HotReloadSupportClass {
-private static final String ID = "b9f1807f-6487-4389-8fc8-a01719de0af3";
-}
--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++
@@ -1,5 +0,0 @@
-package org.kie.kogito.app;
-@io.quarkus.runtime.Startup()
-public class HotReloadSupportClass {
-private static final String ID = "31d033b6-4069-46ae-80b6-836cba877b0a";
-}
